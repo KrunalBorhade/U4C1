6cc4e798e06af811cda1586caa68f270969b7fd7
KrunalBorhade <krunalborhade2897@gmail.com>
2022-03-07 12:47:57 +0530

    final Commit

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 const express = require('express')
 
 const app = express() 
+
+app.use(logger)
+app.get("/books", (req,res)=>{
+
+   return res.send({route: '/books'})
+})
+
+app.get("/libraries",checkPermission("libraries"), (req,res)=>{
+   
+   return res.send({route: "/libraries",permission: true})
+})
+
+app.get("/authors",checkPermission("authors"), (req,res)=>{
+   return res.send({ route: "/authors",permission: true})
+})
+
+function logger(req,res,next){
+    console.log(req.path)
+    console.log("Before initialization")
+    next()
+}
+
+function checkPermission(check){
+    console.log("Before initialization checkPermission")
+    return function logger(req,res,next){
+    if(check=="libraries"){
+        next()
+    }
+    else if(check=="libraries"){
+        next()
+    }
+    // next()
+}
+}
+
+app.listen(5500,()=>{
+    console.log("Listening to port 5500")
+})
